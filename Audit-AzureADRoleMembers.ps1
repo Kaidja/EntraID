f73f2fdc7a757ba70138df354251c8a7b7e72587
Kaido Järvemets <me@kaidojarvemets.ee>
2021-10-14 18:45:51 +0300

    Update Audit-AzureADRoleMembers.ps1

--- Audit-AzureADRoleMembers.ps1
+++ Audit-AzureADRoleMembers.ps1
@@ -2,13 +2,16 @@
 Connect-AzureAD
 
 $AuditData = @()
+
+#Get the Azure AD Directory Roles
 $AzureADRoles = Get-AzureADDirectoryRole
 foreach($Role in $AzureADRoles){
-    
+
+    #Get specific Azure AD Role members    
     $GroupMembers = Get-AzureADDirectoryRoleMember -ObjectId $Role.ObjectId
 
     foreach($Member in $GroupMembers){
-
+        #Check the member type
         If($Member.ObjectType -eq "ServicePrincipal"){
             
             $ObjectType = "ServicePrincipal"
@@ -23,9 +26,11 @@ foreach($Role in $AzureADRoles){
             DisplayName = $Member.DisplayName
         
         }
+        #Create PowerShell object and save the information
         $Object = New-Object -TypeName PSObject -Property $UserProperties
         $AuditData += $Object
     }
 }
 
+#Print out the data
 $AuditData
